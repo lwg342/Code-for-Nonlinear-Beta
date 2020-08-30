8460d199d9da901055d01f869b23a52acb93c353
Weiguang <47528654+lwg342@users.noreply.github.com>
2020-08-30 11:03:32 +0100

    v0.9 of NLBetaTest classs

--- nonlinear_beta_test_factor_zoo.py
+++ nonlinear_beta_test_factor_zoo.py
@@ -1,4 +1,5 @@
 # %% Import Packages
+
 import pandas as pd
 import numpy as np
 from scipy import linalg as LA
@@ -9,44 +10,12 @@ from statsmodels.distributions.empirical_distribution import ECDF
 from myfunc import OLSRegression, kernel_test, loc_poly, my_bootstrap
 import time
 DATA_Folder = '/Users/lwg342/OneDrive - University of Cambridge/Utility/Data/Stock data/FACTOR Zoo Data/data/'
-# %% [markdown]
-# # Import Data
-# 1. Return data. Here I use S&P500 constituents, can be extended to more stocks
-# 2. Import FACTOR data from FACTOR Zoo
-# %% Import and cleaning data
-# RET = pd.read_csv("cleaned_RET.csv", index_col='date')
-RET = pd.read_csv(
-    DATA_Folder + "port202.csv", index_col=0, header=None)
-RET.index = pd.to_datetime(RET.index, format='%Y%m')
-RET = RET/100
-RET.iloc[0:5, 0:5]
-# %% 5*5 portfolio
-# RET = pd.read_csv(
-#     DATA_Folder + "port_5x5.csv", header=None)
-# RET = RET.drop([0], axis=1)
-# RET.index = pd.date_range("1976-07-31", "2017-12-31", freq='M')
-# RET.iloc[0:5, 0:5]
-# %% Import FACTOR Data
-FACTOR = pd.read_csv(
-    'factors_zoo.csv')
-FACTOR.rename(columns={'  Date': 'date'}, inplace=True)
-FACTOR.date = pd.to_datetime(FACTOR.date, format='%Y%m%d')
-FACTOR = FACTOR.pivot_table(index='date')
-FACTOR = FACTOR.dropna(axis=1)
-FACTOR.iloc[0:5, 0:5]
-# %% [markdown]
-# # Individual test of nonlinearity
-# %%
-RET_EXCESS = RET - np.array([FACTOR.RF]).T
-FACTOR.RF.iloc[0:5]
-RET_EXCESS.iloc[0:5, 0:5]
-average_ret = np.array(RET.mean())
-average_excess_ret = np.array(RET_EXCESS.mean())
 # %% Apply to all factors
 
 
 class NLBetaTest():
     def __init__(self, FACTOR, RET_EXCESS, Result=[]):
+        from tabulate import tabulate
         self.FACTOR = FACTOR
         self.RET_EXCESS = RET_EXCESS
         self.Result = Result
@@ -59,8 +28,14 @@ class NLBetaTest():
         print('Header of FACTORs\n', self.FACTOR.iloc[0:3, 0:3], '\n')
         print('Header of Excess Return\n',
               self.RET_EXCESS.iloc[0:3, 0:3], '\n')
-        param_list =
-        print('Result is', self.Result, '\n')
+        param_list = [
+            ['Cross_sectional Regression has intercept', self.with_intercept],
+            ['Bootstrap Iterations', self.bootstrap_iteration],
+            ['Estimating Beta With', self.estimating_period],
+            ['Estimating Average Excess Return with', self.testing_period],
+        ]
+        print('Result is\n', self.Result, '\n')
+        print(tabulate(param_list, ['Parameter', 'Value']))
 
     def beta_estimate(self):
         # Estimating_period is a list of Booleans indicating which periods are used for estimating the betas
@@ -99,91 +74,54 @@ class NLBetaTest():
         return
 
     def save_result(self, name):
-        address = '_'.join([name] + ['.csv'])
+        address = name + '.csv'
         pd.Dataframe(self.Result, index_col=0).to_csv(address)
+# %% [markdown]
+# # Import Data
+# 1. Return data. Here I use S&P500 constituents, can be extended to more stocks
+# 2. Import FACTOR data from FACTOR Zoo
+# %% Import and cleaning data
+# RET = pd.read_csv("cleaned_RET.csv", index_col='date')
+RET = pd.read_csv(
+    DATA_Folder + "port202.csv", index_col=0, header=None)
+RET.index = pd.to_datetime(RET.index, format='%Y%m')
+RET = RET/100 # This is special to this 202 return data
+RET.iloc[0:5, 0:5]
+
+# %% 
+# %% 5*5 portfolio
+# RET = pd.read_csv(
+#     DATA_Folder + "port_5x5.csv", header=None)
+# RET = RET.drop([0], axis=1)
+# RET.index = pd.date_range("1976-07-31", "2017-12-31", freq='M')
+# RET.iloc[0:5, 0:5]
+# %% Import FACTOR Data
+FACTOR = pd.read_csv(
+    'factors_zoo.csv')
+FACTOR.rename(columns={'  Date': 'date'}, inplace=True)
+FACTOR.date = pd.to_datetime(FACTOR.date, format='%Y%m%d')
+FACTOR = FACTOR.pivot_table(index='date')
+FACTOR = FACTOR.dropna(axis=1)
+FACTOR.iloc[0:5, 0:5]
+# %% [markdown]
+# # Individual test of nonlinearity
+# %%
+RET_EXCESS = RET - np.array([FACTOR.RF]).T
+FACTOR.RF.iloc[0:5]
+RET_EXCESS.iloc[0:5, 0:5]
+average_ret = np.array(RET.mean())
+average_excess_ret = np.array(RET_EXCESS.mean())
 
 
-# def original_nonlinear_beta_test(baseline_factor, FACTOR=FACTOR, RET_EXCESS=RET_EXCESS, with_intercept=1):
-#     Result = []
-#     tic = time.time()
-#     count = 0
-#     for i in FACTOR.columns[0:5]:
-#         if i in baseline_factor:
-#             Result.append(
-#                 [i, '-----', '-----', '-----'])
-#         else:
-#             select = baseline_factor + [i]
-#             beta = np.array(OLSRegression(
-#                 np.array(FACTOR[select]), RET_EXCESS).beta_hat().iloc[:, 1:])
-#             Tn, Critical_left, Critical_right = my_bootstrap(
-#                 beta, average_excess_ret, B=500, intercept=with_intercept)
-#             Result.append([i, Tn, Critical_left, Critical_right])
-#             if Tn < Critical_left or Tn > Critical_right:
-#                 print("The factor", i, "is nonlinear!")
-#                 count = count+1
-#     print("Total Elapsed Time =", time.time() - tic)
-#     print('Nonlinear Count =', count)
-#     return Result
 # %% Test
-A = NLBetaTest(FACTOR, RET_EXCESS)
-A.report()
-A.one_model(['MktRf']).report()
+Model1 = NLBetaTest(FACTOR, RET_EXCESS)
+Model1.report()
+Model1.test_model(['MktRf'])
 # %% Different baseline factors.
-# Individual
-baseline_factor = []
-Result = nonlinear_beta_test(baseline_factor)
-pd.DataFrame(Result).to_csv('Result_individual.csv')
-# %% Market As baseline
-baseline_factor = ['MktRf']
-[Tn, Critical_left, Critical_right] = nonlinear_beta_test(baseline_factor)
-pd.DataFrame([Tn, Critical_left, Critical_right]
-             ).to_csv('Result_baseline_mktrf.csv')
-# %% FF3 as basline
-baseline_factor = ['MktRf', 'HML', 'SMB']
-[Tn, Critical_left, Critical_right] = nonlinear_beta_test(baseline_factor)
-pd.DataFrame([Tn, Critical_left, Critical_right]
-             ).to_csv('Result_baseline_ff3.csv')
-
 # %% Taming the FACTOR Zoo chooses 4 FACTORs:
 # SMB, nxf, chcsho, pm
 baseline_factor = ['SMB', 'nxf', 'chcsho', 'pm']
 [Tn, Critical_left, Critical_right] = nonlinear_beta_test(baseline_factor)
 pd.DataFrame([Tn, Critical_left, Critical_right]
              ).to_csv('Result_baseline_TFZ4.csv')
-# %% Taming the FACTOR Zoo chooses 4 FACTORs:
-# SMB, nxf, chcsho, pm
-baseline_factor = ['SMB', 'nxf', 'chcsho', 'pm']
-[Tn, Critical_left, Critical_right] = nonlinear_beta_test(baseline_factor)
-pd.DataFrame([Tn, Critical_left, Critical_right]
-             ).to_csv('Result_baseline_TFZ4.csv')
-# %%
-result = pd.read_csv('result_individual_without_intercept.csv')
-(result.iloc[0, 1:] < result.iloc[1, 1:]).sum()
-(result.iloc[0, 1:] > result.iloc[2, 1:]).sum()
-# %%
-result = pd.read_csv('result_baseline_mktrf_without_intercept.csv')
-(result.iloc[0, :] < result.iloc[1, :]).sum()
-(result.iloc[0, :] > result.iloc[2, :]).sum()
 
-# %%
-result = pd.read_csv('result_baseline_ff3_without_intercept.csv')
-(result.iloc[0, :] < result.iloc[1, :]).sum()
-(result.iloc[0, :] > result.iloc[2, :]).sum()
-
-# %%
-result = pd.read_csv('result_baseline_TFZ4.csv')
-(result.iloc[0, :] < result.iloc[1, :]).sum()
-(result.iloc[0, :] > result.iloc[2, :]).sum()
-
-
-# %%
-i = 3
-plt.figure()
-plt.scatter(beta[:, i], average_excess_ret)
-plt.scatter(beta[:, i], OLSRegression(
-    beta, average_excess_ret).y_hat(intercept=0))
-plt.scatter(beta[:, i], OLSRegression(beta, average_excess_ret).y_hat())
-# plt.scatter(beta[:,i], loc_poly(average_excess_ret, beta[:,i]))
-print(a, b, c)
-
-# %%
